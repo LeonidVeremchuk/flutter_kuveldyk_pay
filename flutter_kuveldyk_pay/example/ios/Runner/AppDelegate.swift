b3e4bac9e503f4b6b83c363716177ce519d69ea2
Yegor Pavlyukov <backspaceegor@gmail.com>
2019-06-05 18:09:07 +0300

    integrate SDK for iOS

--- flutter_kuveldyk_pay/example/ios/Runner/AppDelegate.swift
+++ flutter_kuveldyk_pay/example/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import Braintree
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
   ) -> Bool {
+    BTAppSwitch.setReturnURLScheme("com.snailapp.flutterKuveldykPayExample.payments")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
